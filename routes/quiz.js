867e918f90668ebeb24138e494f6eb37f0778fb6
natalielim <natalielim@gmail.com>
2019-03-30 16:36:01 -0700

    tweets appear on new quiz page

--- routes/quiz.js
+++ routes/quiz.js
@@ -38,10 +38,47 @@ router.get('/', auth.requireLogin, (req, res, next) => {
 router.get('/new', auth.requireLogin, (req, res, next) =>{
   User.findById(req.params.userId, function(err, quiz) {
     if(err) { console.error(err);}
-
-    res.render('quiz/new');
   });
+
+  var Twitter = require('twitter');
+
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  var candidates = [
+    "JohnDelaney", "AndrewYang", "ewarren", "JulianCastro", "TulsiGabbard",
+    "SenGillibrand", "KamalaHarris", "PeteButtigieg", "CoryBooker", "amyklobuchar",
+    "BernieSanders", "GovInslee", "Hickenlooper", "BetoORourke", "realDonaldTrump",
+    "WayneMessam", "marwilliamson", "Joebiden", "RepSwalwell", "GovBillWeld", "OfficialMcAfee" ];
+  var r = Math.floor((Math.random() * 21) + 1);
+  var name = candidates[r];
+  console.log(name);
+
+  var params = {
+    screen_name: name,
+    count: 100,
+    tweet_mode: 'extended',
+    // in_reply_to_status_id: null,
+    // retweeted: false
+  };
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var random = Math.floor((Math.random() * 100));
+    var text = tweets[random]["full_text"];
+    for(i = 0; i < text.length; i++) {
+      if(text.substring(i,i+5)==("&amp;")) {
+        text= text.substring(0,i) + "&" + text.substring(i+5);
+      }
+    }
+    res.render('quiz/new', { title: 'Woke', text: text});
+  });
+
 });
+
 //
 // // /* GET quiz by ID. */
 // // router.get('/:id', auth.requireLogin, (req, res, next) => {
