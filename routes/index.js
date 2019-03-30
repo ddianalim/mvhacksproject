790262a53715f7b9658fd53fd94f403b4d622ac2
dianalimnos <dianalim1117@gmail.com>
2019-03-30 13:29:21 -0700

    login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Woke' });
+// set layout variables
+router.use(function(req, res, next) {
+  res.locals.title = "Woke";
+  res.locals.currentUserId = req.session.userId;
+  res.locals.username = req.session.username;
+
+  next();
+});
+
+// home page
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// login
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+// POST login
+router.post('/login', (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return next(next_error);
+    } else {
+      req.session.userId = user._id;
+      req.session.username = user.username;
+
+      return res.redirect('/');
+    }
+  });
+});
+
+// logout
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) return next(err);
+    });
+  }
+
+  return res.redirect('/');
 });
 
 module.exports = router;
