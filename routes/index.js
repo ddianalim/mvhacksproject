9712406a82796059b4e8bf8a52d1b644630ebf7c
20mariade <49100732+20mariade@users.noreply.github.com>
2019-03-30 09:51:59 -0700

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Woke!' });
+  res.render('index', { title: 'Woke!!' });
 });
 
 module.exports = router;
+ 
