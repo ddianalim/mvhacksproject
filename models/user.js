bdcc11727dbc7a93b5cbe3894f69a7ba06f9a412
dianalimnos <dianalim1117@gmail.com>
2019-03-30 18:05:37 -0700

    yessir

--- models/user.js
+++ models/user.js
@@ -4,7 +4,28 @@ const bcryptjs = require('bcryptjs');
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  JohnDelaney: { type: Number, default: 0 },
+  AndrewYang: { type: Number, default: 0 },
+  ewarren: { type: Number, default: 0 },
+  JulianCastro: { type: Number, default: 0 },
+  TulsiGabbard: { type: Number, default: 0 },
+  SenGillibrand: { type: Number, default: 0 },
+  KamalaHarris: { type: Number, default: 0 },
+  PeteButtigieg: { type: Number, default: 0 },
+  CoryBooker: { type: Number, default: 0 },
+  amyklobuchar: { type: Number, default: 0 },
+  BernieSanders: { type: Number, default: 0 },
+  GovInslee: { type: Number, default: 0 },
+  Hickenlooper: { type: Number, default: 0 },
+  BetoORourke: { type: Number, default: 0 },
+  realDonaldTrump: { type: Number, default: 0 },
+  WayneMessam: { type: Number, default: 0 },
+  marwilliamson: { type: Number, default: 0 },
+  Joebiden: { type: Number, default: 0 },
+  RepSwalwell: { type: Number, default: 0 },
+  GovBillWeld: { type: Number, default: 0 },
+  OfficialMcAfee: { type: Number, default: 0 },
 });
 
 UserSchema.pre('save', function(next) {
